b3e9e3ca3c9fa0c68480df443418b90fbe297ae3
sulovigo <sulovigo@fit.cvut.cz>
2022-12-08 14:30:46 +0100

    Updated views

--- vmm/urls.py
+++ vmm/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from vmm.views import Index
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Index.as_view(), name='index'),
 ]
--- vmm/views.py
+++ vmm/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.views import View
-
+from distance_computation import compute_distance
 
 class Index(View):
     template = 'index.html'
 
     def get(self, request):
         return render(request, self.template)
+
+    def post(self, request):
+        handle_uploaded_file(request.FILES['query_img'])
+
+        return render(request, self.template)
+
+
+def handle_uploaded_file(f):
+    file_type = f.name.split('.')[-1]
+    with open('images/uploaded.' + file_type, 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
